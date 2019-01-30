58fe77f2255e2808dbb9b6986868808632735f22
Sean Kelly <shonkelli@gmail.com>
2019-01-30 19:27:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 function takeANumber(lineArray, newName){
   lineArray.push(newName)
   var linePosition = lineArray.indexOf(newName) + 1
-  // var getPosition = function(array, name) {
-  //   return array.indexOf(name) + 1
-  // };
-  // var msg =  "Welcome, " + newName + ". You are number "+ getPosition(lineArray, newName) +" in line."
   var msg = `Welcome, ${newName}. You are number ${linePosition} in line.`
   return msg
-};
+}
 
 function nowServing(lineArray){
   if (lineArray.length < 1) {
@@ -16,7 +12,7 @@ function nowServing(lineArray){
     var currentCustomer = lineArray.shift()
     return "Currently serving " + currentCustomer + "."
   }
-};
+}
 
 var currentLine = function(lineArray){
   if (lineArray.length < 1) {
